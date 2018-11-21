d03a57f3c8bba9dc92ab90def8a8e423a72a60ec
Fricken Hamster <frickenhamster@gmail.com>
2018-11-20 21:45:21 -0800

    update info

---
+++ precache-manifest.c608f1a8f07323add72e669630bfa964.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "5a7fbfd1ab1e05337352",
+    "url": "/static/css/main.d73dc435.chunk.css"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fc9ac6758f771a578e3c",
+    "url": "/static/js/1.fc9ac675.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a7fbfd1ab1e05337352",
+    "url": "/static/js/main.5a7fbfd1.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fc9ac6758f771a578e3c",
+    "url": "/static/css/1.abaf8013.chunk.css"
+  },
+  {
+    "revision": "0ccb7ab3ce306350d265a735b7f3ed97",
+    "url": "/index.html"
+  }
+];
